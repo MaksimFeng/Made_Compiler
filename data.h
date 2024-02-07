3fdc448058420920d4c17c1a36cba67ac68572f2
Kai Feng <maksimfengk@outlook.com>
2024-02-07 23:01:54 +0000

    finish the process of intiger and symbol

---
+++ data.h
@@ -0,0 +1,11 @@
+#ifndef extern_
+    #define extern_ extern  
+#endif
+// Global variables
+// Copyright (c) 2019 Warren Toomey, GPL3
+
+// Global variables
+
+extern_ int Line;
+extern_ int Putback;
+extern_ FILE *Infile;
